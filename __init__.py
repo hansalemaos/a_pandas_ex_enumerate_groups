e22eeddae1a23d335a66c0130fa826b56f6c29ab
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2022-12-17 14:46:50 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from typing import Union
 
 import pandas as pd
 from pandas.core.frame import DataFrame, Series
+from useful_functions_easier_life import ignore_exceptions
 
 
 def series_to_dataframe_convert(
@@ -27,30 +28,77 @@ def enumerate_groups(dframe, enumerated_column, column_to_enumerate):
     gdf, isseries = series_to_dataframe_convert(dframe)
     if isseries:
         col = gdf.columns[0]
-    soind = gdf[col].value_counts().index.sort_values(0)
-    soind1_ = soind[-1]
-    try:
-        soind2_ = soind[-2]
-    except Exception:
-        soind2_ = soind[-1]
-    nafill = soind1_ + soind2_
-    gdf = gdf.sort_values(by=col)
-    df = gdf.copy()
-    df[col] = df[col].fillna(nafill)
-    dfgr = df.groupby(col, dropna=False)[col]
-    ppa = dfgr.apply(
-        lambda x: (
-            [list(dfgr.groups.keys()).index(x.name)] * len(dfgr.groups.get(x.name))
+    prefix = "XXXXXXXXXgroup_"
+    namesep = ""
+    return gdf.d_enumerate_groups_in_multiple_columns(
+        columns=[[col]], prefix=prefix, name_sep=namesep
+    ).rename(columns={f"{prefix}{column_to_enumerate}": enumerated_column})
+
+
+def enumerate_all_columns_groups(df, prefix="group_"):
+    df = df.reset_index(drop=True)
+    dftoog = (
+        pd.concat(
+            [
+                df,
+                pd.concat(
+                    [
+                        ignore_exceptions(
+                            lambda: df.groupby(col).ngroup(),
+                            exception_value=pd.Series([None] * len(df)),
+                        )
+                        .to_frame()
+                        .rename(columns={0: f"{prefix}{col}"})
+                        for col in df.columns
+                    ],
+                    axis=1,
+                ),
+            ],
+            axis=1,
         )
+        .copy()
+        .fillna(pd.NA)
     )
-    wholeg = ppa.reset_index(drop=True).explode(0).reset_index(drop=True)
-    wholeg.index = df.index.__array__().copy()
-    gdf[enumerated_column] = wholeg.copy()
-    return gdf
+    return dftoog
+
+
+def enumerate_multiple_columns(df, columns, prefix="group_", name_sep="_"):
+    groupbycolumns = columns.copy()
+    # groupbycolumns = [['x', 'y'], ['vor_region', 'vor_vertices', 'vor_vertices_coords']]
+
+    df = df.reset_index(drop=True)
+    dftoog = (
+        pd.concat(
+            [
+                df,
+                pd.concat(
+                    [
+                        ignore_exceptions(
+                            lambda: df.groupby(col).ngroup(),
+                            exception_value=pd.Series([None] * len(df)),
+                        )
+                        .to_frame()
+                        .rename(
+                            columns={
+                                0: f"{prefix}{f'{name_sep}'.join([str(p) for p in col])}"
+                            }
+                        )
+                        for col in groupbycolumns
+                    ],
+                    axis=1,
+                ),
+            ],
+            axis=1,
+        )
+        .copy()
+        .fillna(pd.NA)
+    )
+
+    return dftoog
 
 
 def pd_add_enumerate_group():
     DataFrame.ds_enumerate_groups = enumerate_groups
     Series.ds_enumerate_groups = enumerate_groups
-
-
+    DataFrame.d_enumerate_all_groups_in_all_columns = enumerate_all_columns_groups
+    DataFrame.d_enumerate_groups_in_multiple_columns = enumerate_multiple_columns
